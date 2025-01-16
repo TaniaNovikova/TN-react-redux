251b7364f3e947a16e81c0bc9131bb0f48760095
TaniaNovikova <tania.novikov@web.de>
2025-01-16 19:37:38 +0100

    add feedbackform

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Users from "./pages/UsersApp/Users/Users"
 //lessons
 // import Lesson16 from "./lessons/Lesson16/Lesson16"
 //import Lesson17 from "./lessons/Lesson17/Lesson17"
+import Lesson18 from "./lessons/Lesson18/Lesson18"
+
 
 
 //homeworks
 //import Homework16 from "./homeworks/Homework16.tsx/Homework16"
-import Homework17 from "./homeworks/Homework17/Homework17"
+//import Homework17 from "./homeworks/Homework17/Homework17"
 
 //consultations
 // import Consultation08 from "./consultations/Consultation08/Consultation08"
@@ -32,7 +34,8 @@ const App = () => {
       </Layout> */}
       {/* <Homework16/> */}
       {/* <Lesson17/> */}
-      <Homework17/>
+      {/* <Homework17/> */}
+      <Lesson18/>
     </BrowserRouter>
   )
 }
---
+++ src/lessons/Lesson18/Lesson18.tsx
@@ -0,0 +1,9 @@
+import FeedbackForm from "../../components/FeedbackForm/FeedbackForm";
+import RandomJokes from "../../components/RandomJokes/RandomJokes";
+import { Lesson18Wrapper } from "./styles";
+
+function Lesson18 (){
+  return <Lesson18Wrapper><FeedbackForm/></Lesson18Wrapper>
+}
+
+export default Lesson18;
